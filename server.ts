128e067006032837570a5c531d54edcbf41e899e
Jannis Jüngert <73344489+juengeja@users.noreply.github.com>
2022-04-07 18:35:34 +0200

    update

--- server.ts
+++ server.ts
@@ -2,5 +2,11 @@ import { opine } from "https://deno.land/x/opine@2.1.4/mod.ts";
 
 const app = opine();
 
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
-console.log("Hello Console!")
+app.listen(
+  3000,
+  () => console.log("server has started on http://localhost:3000 🚀"),
+);
